86e9296f1cf227fe4986fc73527894ce4645d59c
alexandra <silvaalexandra123987@gmail.com>
2026-08-06 17:25:36 -0300

    cart update items

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -14,6 +14,8 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
+
+  
 
   cartSummaryHTML += `
     <div class="cart-item-container
@@ -108,5 +110,22 @@ document.querySelectorAll('.js-delete-link')
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+
+      updateCartQuantity();
     });
   });
+
+  function updateCartQuantity(){
+    
+    const cartQuantity = calculateCartQuantity();
+    
+    document.querySelector('.js-return-to-home-link')
+      .innerHTML = `${cartQuantity} items`;
+
+  }
+
+
+  updateCartQuantity();
+
+
+  
