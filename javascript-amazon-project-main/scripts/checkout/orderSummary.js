c9a86f423ca0c3fc0a52b012cb1643f684475ca2
alexandra <silvaalexandra123987@gmail.com>
2026-08-14 09:03:56 -0300

    renderCheckoutHeader with MVC

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -11,6 +11,8 @@ import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
+
 
 
 export function renderOrderSummary() {
@@ -152,13 +154,8 @@ export function renderOrderSummary() {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-        container.remove();
-
-        updateCartQuantity();
-
+        renderCheckoutHeader();
+        renderOrderSummary();
         renderPaymentSummary();
       });
     });
@@ -171,6 +168,8 @@ export function renderOrderSummary() {
         .innerHTML = `${cartQuantity} items`;
 
     }
+
+    
 
 
     document.querySelectorAll('.js-update-link')
