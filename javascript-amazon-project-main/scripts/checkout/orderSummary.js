b10dae6b0abdb8314b692ba329673d52e9808191
alexandra <silvaalexandra123987@gmail.com>
2026-08-19 08:33:58 -0300

    create automated tests for orderSummary

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -41,6 +41,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
       <div class="cart-item-container
+        js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date js-delivery-date">
           Delivery date: ${dateString}
@@ -57,7 +58,7 @@ export function renderOrderSummary() {
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div                  class="product-quantity                    js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
               </span>
@@ -70,7 +71,7 @@ export function renderOrderSummary() {
               data-product-id="${matchingProduct.id}">
                 Save
               </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -158,9 +159,6 @@ export function renderOrderSummary() {
     function updateCartQuantity(){
       
       const cartQuantity = calculateCartQuantity();
-      
-      document.querySelector('.js-return-to-home-link')
-        .innerHTML = `${cartQuantity} items`;
 
     }
 
